8a447e04ca1e021e2e55ba6679c14f675e76787a
Deca Fujita <decafujita@PackMac.local>
2021-08-23 11:23:36 -0400

    added navbar

--- src/Palette.jsx
+++ src/Palette.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
 import './Palette.css';
 
+
 class Palette extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {level: 500}; 
+    }
+
+    changeLevel = (level) => {
+        this.setState({level})
+    }
     render() {
-        const colorBoxes = this.props.palette.colors[300].map(color => (
+        const { colors } = this.props.palette;
+        const { level } = this.state; 
+        const colorBoxes = colors[level].map(color => (
             <ColorBox background={color.hex} name={color.name}/>
         )); 
         return(
             <div className='Palette'>
+                <Navbar level={level} changeLevel={this.changeLevel} />
                 <div className='Palette-colors'>
                 {colorBoxes}
                 </div>
