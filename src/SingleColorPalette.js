6cda8f58cfbd4c2a2adb950233697a5066517a01
Deca Fujita <M11@MiniAir.local>
2021-08-26 15:39:32 -0300

    refactor colourbox css

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -39,7 +39,7 @@ class SingleColorPalette extends React.Component {
                 key={color.name}
                 name={color.name}
                 background={color[format]}
-                showLink={false} />
+                showingFullPalette={false} />
         ))
         return (
             <div className='SingleColorPalette Palette'> 
